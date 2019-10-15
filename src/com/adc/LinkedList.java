4b44231827ec0caf9664faf644b44da8108a26ba
yanghanqing <imyanghanqing@gmail.com>
2019-10-15 23:33:26 +0800

    init

---
+++ src/com/adc/LinkedList.java
@@ -0,0 +1,15 @@
+package com.adc;
+
+/**
+ * 链表
+ */
+public class LinkedList {
+
+    private Node head;
+    private Node tail;
+
+    public static class Node {
+        private String data;
+        private Node next;
+    }
+}

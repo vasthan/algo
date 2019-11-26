9143be6c735836a0b8c9765a3abcf8a31fb7ae36
yanghanqing <imyanghanqing@gmail.com>
2019-11-26 21:28:01 +0800

    参数检查

--- src/main/java/com/adc/sort/InsertionSort.java
+++ src/main/java/com/adc/sort/InsertionSort.java
@@ -4,22 +4,24 @@ import java.util.Arrays;
 
 public class InsertionSort {
 
-    public static void sort(int[] arr, int n) {
-
-        for (int i = 1; i < n; i++) {
-            int e = arr[i];
-            int j;
-            for (j = i; j > 0 && arr[j - 1] > e; j--) {
+    public static void sort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int i = 1; i < arr.length; i++) {
+            int v = arr[i];
+            int j = i;
+            for (; j > 0 && arr[j - 1] > v; j--) {
                 arr[j] = arr[j - 1];
             }
-            arr[j] = e;
+            arr[j] = v;
         }
     }
 
     public static void main(String[] args) {
         int[] arr = SortHelper.generateIntArray(20, 1000);
         System.out.println(Arrays.toString(arr));
-        sort(arr, arr.length);
+        sort(arr);
         System.out.println(Arrays.toString(arr));
     }
 }

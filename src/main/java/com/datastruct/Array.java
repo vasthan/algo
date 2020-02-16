2b23a30e3b5323c264ca28d2889479ff9af4ffb2
yanghanqing <imyanghanqing@gmail.com>
2020-02-16 21:34:07 +0800

    dynamic array

---
+++ src/main/java/com/datastruct/Array.java
@@ -0,0 +1,144 @@
+package com.datastruct;
+
+import java.util.Objects;
+
+public class Array<E> {
+    private E[] data;
+    private int size;
+
+    public Array(int capacity) {
+        this.data = (E[]) new Object[capacity];
+        this.size = 0;
+    }
+
+    public Array() {
+        this(10);
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getCapacity() {
+        return data.length;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // 数组尾部添加元素
+    public void addLast(E e) {
+        add(size, e);
+    }
+
+    // 数组头部添加元素
+    public void addFirst(E e) {
+        add(0, e);
+    }
+
+    // 数组指定位置添加元素
+    public void add(int index, E e) {
+        if (index < 0 || index > size)
+            throw new IllegalArgumentException("Add failed. Require index >= 0 and index <= size");
+
+        if (size == data.length) {
+            resize(2 * data.length);
+        }
+
+        for (int i = size - 1; i >= index; i--)
+            data[i + 1] = data[i];
+
+        data[index] = e;
+        size++;
+    }
+
+    public E get(int index) {
+        if (index < 0 || index >= size)
+            throw new IllegalArgumentException("Get failed. Index is illegal.");
+        return data[index];
+    }
+
+    // 设置数组指定索引位置的值，并返回之前的值
+    public E set(int index, E e) {
+        if (index < 0 || index >= size)
+            throw new IllegalArgumentException("Get failed. Index is illegal.");
+        E oldValue = data[index];
+        data[index] = e;
+        return oldValue;
+    }
+
+    public boolean contains(E e) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(e)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 查找数组某个值对应的索引，如果不存在，返回-1
+    public int find(E e) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(e)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // 删除指定索引位置的元素
+    public E remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Remove failed. Index is illegal.");
+        }
+        E oldValue = data[index];
+        for (int i = index + 1; i < size; i++) {
+            data[i - 1] = data[i];
+        }
+        size--;
+        data[size] = null; //loitering object != memory leak
+        if (data.length / 4 == size && data.length / 2 != 0) {
+            resize(data.length / 2);
+        }
+        return oldValue;
+    }
+
+    public E removeFirst() {
+        return remove(0);
+    }
+
+    public E removeLast() {
+        return remove(size - 1);
+    }
+
+    public void removeElement(E e) {
+        int index = find(e);
+        if (index != -1) {
+            remove(index);
+        }
+    }
+
+    private void resize(int newCapacity) {
+        E[] newData = (E[]) new Object[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newData[i] = data[i];
+        }
+        data = newData;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+        str.append(String.format("Array: size = %d, capacity = %d\n", size, data.length));
+        str.append("[");
+        for (int i = 0; i < size; i++) {
+            str.append(data[i]);
+            if (i != size - 1) {
+                str.append(", ");
+            }
+        }
+        str.append("]");
+        return str.toString();
+    }
+}

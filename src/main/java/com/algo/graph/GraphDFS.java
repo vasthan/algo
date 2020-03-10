bc8b5ce48f6c70634c8695a78530fe8dc77f7e34
yanghanqing <imyanghanqing@gmail.com>
2020-03-10 22:26:37 +0800

    preOrder dfs and postOrder dfs

--- src/main/java/com/algo/graph/GraphDFS.java
+++ src/main/java/com/algo/graph/GraphDFS.java
@@ -9,33 +9,44 @@ public class GraphDFS {
 
     private Graph g;
     // 存放深度优先遍历的结果
-    private List<Integer> order = new ArrayList<>();
+    private List<Integer> preOrder = new ArrayList<>();
+    private List<Integer> postOrder = new ArrayList<>();
     // 标记已遍历过的点
     private boolean[] visited;
 
     public GraphDFS(Graph g) {
         this.g = g;
         this.visited = new boolean[g.getV()];
-        dfs(0);
+        for (int v = 0; v < g.getV(); v++) {
+            if (!visited[v]) {
+                dfs(v);
+            }
+        }
     }
 
     private void dfs(int v) {
         visited[v] = true;
-        order.add(v);
+        preOrder.add(v);
         for (int w : g.adj(v)) {
             if (!visited[w]) {
                 dfs(w);
             }
         }
+        postOrder.add(v);
     }
 
-    public List<Integer> order() {
-        return order;
+    public List<Integer> preOrder() {
+        return preOrder;
+    }
+
+    public List<Integer> postOrder() {
+        return postOrder;
     }
 
     public static void main(String[] args) {
         Graph g = new Graph("src/main/resources/g.txt");
         GraphDFS dfs = new GraphDFS(g);
-        System.out.println(dfs.order);
+        System.out.println(dfs.preOrder());
+        System.out.println(dfs.postOrder());
     }
 }

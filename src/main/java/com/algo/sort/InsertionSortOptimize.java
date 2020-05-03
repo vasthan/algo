9fb876131a692ec0d19a68c8b49335d1c9ea2cd2
yanghanqing <imyanghanqing@gmail.com>
2020-05-03 13:21:24 +0800

    update

--- src/main/java/com/algo/sort/InsertionSortOptimize.java
+++ src/main/java/com/algo/sort/InsertionSortOptimize.java
@@ -1,7 +1,5 @@
 package com.algo.sort;
 
-import com.util.ArrayUtils;
-
 // 插入排序优化
 public class InsertionSortOptimize {
 
@@ -17,22 +15,5 @@ public class InsertionSortOptimize {
             }
             arr[j] = e;
         }
-    }
-
-    public static void sort(Comparable[] arr, int l, int r) {
-        for (int i = l + 1; i <= r; i++) {
-            Comparable e = arr[i];
-            int j = i;
-            for (; j > 0 && arr[j - 1].compareTo(arr[j]) > 0; j--) {
-                arr[j] = arr[j - 1];
-            }
-            arr[j] = e;
-        }
-    }
-
-    public static void main(String[] args) {
-        Integer[] arr = ArrayUtils.generateRandomIntArr(10000, 0, Integer.MAX_VALUE);
-        InsertionSortOptimize.sort(arr, 0, arr.length - 1);
-        SortHelper.isSorted(arr);
     }
 }

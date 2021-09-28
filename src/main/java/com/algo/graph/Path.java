0e75fe3b54a022c51331b69f2d725e5a31540d9c
yanghanqing <imyanghanqing@gmail.com>
2021-09-28 10:53:02 +0800

    路径问题优化：提前结束递归

--- src/main/java/com/algo/graph/Path.java
+++ src/main/java/com/algo/graph/Path.java
@@ -10,34 +10,39 @@ import java.util.List;
 // 求在图中，源顶点source到目标顶点target的路径
 // 提前结束递归，路径问题优化
 public class Path {
-    private Graph G;
+
+    private Graph g;
+    // source
     private int s;
+    // target
     private int t;
+    // 记录每一个顶点是否访问过，可以优化，用pre数组代替
     private boolean[] visited;
+    // 记录每一个顶点的前一个顶点
     private int[] pre;
 
-    public Path(Graph graph, int source, int target) {
-        graph.validateVertex(source);
-        graph.validateVertex(target);
-        this.G = graph;
-        this.s = source;
-        this.t = target;
-        this.visited = new boolean[graph.getV()];
-        this.pre = new int[graph.getV()];
-        for (int i = 0; i < pre.length; i++) {
-            pre[i] = -1;
-        }
+    // 任意两点间的路径，一次性
+    public Path(Graph g, int s, int t) {
+        g.validateVertex(s);
+        g.validateVertex(t);
+        this.g = g;
+        this.s = s;
+        this.t = t;
+        this.visited = new boolean[g.getV()];
+        this.pre = new int[g.getV()];
+        Arrays.fill(pre, -1);
         dfs(s, s);
+        System.out.println(Arrays.toString(visited));
     }
 
-    // 搜索从顶点v到顶点t是否有路径
     private boolean dfs(int v, int parent) {
         visited[v] = true;
         pre[v] = parent;
         if (v == t) {
+            // 遍历到t，表示有路径
             return true;
         }
-        for (int w : G.adj(v)) {
+        for (int w : g.adj(v)) {
             if (!visited[w]) {
                 if (dfs(w, v)) {
                     return true;
@@ -47,38 +52,39 @@ public class Path {
         return false;
     }
 
+    // s和t是否相联通
     public boolean isConnected() {
         return visited[t];
     }
 
+    // s到t的路径
     public List<Integer> path() {
-        List<Integer> path = new ArrayList<>();
+        List<Integer> p = new ArrayList<>();
         if (!isConnected()) {
-            return path;
+            return p;
         }
         int cur = t;
         while (cur != s) {
-            path.add(cur);
+            p.add(cur);
             cur = pre[cur];
         }
-        path.add(s);
-        Collections.reverse(path);
-        return path;
-    }
-
-    public void printVisited() {
-        System.out.println(Arrays.toString(visited));
+        p.add(s);
+        Collections.reverse(p);
+        return p;
     }
 
     public static void main(String[] args) {
         Graph g = new Graph("src/main/resources/g.txt");
-        Path path = new Path(g, 0, 6);
-        path.printVisited();
-        System.out.println("0 -> 6 : " + path.path());
+        Path path = new Path(g, 0, 1);
+        System.out.println("0 -> 1 is connected? " + path.isConnected());
+        System.out.println("0 -> 1 path: " + path.path());
 
-        path = new Path(g, 0, 1);
-        path.printVisited();
-        System.out.println("0 -> 1 : " + path.path());
+        Path path2 = new Path(g, 0, 6);
+        System.out.println("0 -> 6 is connected? " + path2.isConnected());
+        System.out.println("0 -> 6 path: " + path2.path());
 
+        Path path3 = new Path(g, 0, 5);
+        System.out.println("0 -> 5 is connected? " + path3.isConnected());
+        System.out.println("0 -> 5 path: " + path3.path());
     }
 }
